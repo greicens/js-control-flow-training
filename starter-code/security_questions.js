c6b91de16845ee03b1d5ac25b5ad2a9fd82561d3
greicens <gsilva@mail.ccsf.edu>
2016-11-29 21:00:38 -0800

    created askQuestion funtion

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,6 +1,18 @@
 console.log("security_questions.js loaded");
 
 var securityQuestions = [{question: "what was your first pet's name?", expectedAnswer: "rex"},
-                         {question: "what is your mother's maiden name?", expectedAnswer: "smith"}
+                         {question: "what is your mother's maiden name?", expectedAnswer: "smith"},
                          {question: "where were you born", expectedAnswer: "Brazil"}
                         ];
+
+function askQuestion(){
+  for (var i = 0; i < securityQuestions.length; i++) {
+    var userAnswer = window.prompt(securityQuestions[i].question);
+    if(userAnswer !== securityQuestions[i].expectedAnswer){
+      window.alert("that is not the correct answer");
+      break; //is this a correct use for the key word break?
+    }
+  }
+}
+
+askQuestion();
