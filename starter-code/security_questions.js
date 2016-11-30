b8cdce778fe122d1881f30627c08450bc2760f74
greicens <gsilva@mail.ccsf.edu>
2016-11-29 20:31:25 -0800

    created securityQuestions array and populated it with objects

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,6 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [{question: "what was your first pet's name?", expectedAnswer: "rex"},
+                         {question: "what is your mother's maiden name?", expectedAnswer: "smith"}
+                         {question: "where were you born", expectedAnswer: "Brazil"}
+                        ];
