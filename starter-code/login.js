a967c27975e235eb47accd50dbc3b31c1422a175
greicens <gsilva@mail.ccsf.edu>
2016-11-29 22:09:33 -0800

    created verigyLogin function}

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,22 @@
 console.log("login.js loaded");
+var userLogin = {userName: "x10", password: 1010};
+
+function askPassword(){
+  return window.prompt("Enter password for user " + userLogin.userName);
+}
+
+function verifyLogin(){
+  if(askPassword() !== userLogin.password){
+    var i = 3;
+    while(askPassword() !== userLogin.password && i < 3){
+      window.alert("Password Incorrect!");
+      askPassword();
+      i++;
+    }
+   }else{
+    window.alert("Password Correct!");
+  }
+
+}
+
+verifyLogin();
