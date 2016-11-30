81c493aea5f6f5c112f038198147e950c780dc8c
greicens <gsilva@mail.ccsf.edu>
2016-11-29 22:40:43 -0800

    created sing function

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,21 @@
 console.log("sing.js loaded");
+
+var userAnswer = window.prompt("How many verses of music would like to hear?");
+
+function sing(){
+  for (var i = parseInt(userAnswer); i > 0; i--) {
+    if(i > 1){
+      console.log(i + " bottles of beer on the wall, ");
+      console.log(i + " bottles of beer!");
+      console.log("Take one down and pass it around");
+      console.log(i - 1 + " bottles of beer on the wall!");
+    }else if(i === 1){
+      console.log(i + " bottle of beer on the wall, ");
+      console.log(i + " bottle of beer!");
+      console.log("Take one down and pass it around");
+      console.log("No more bottles of beer");
+    }
+  }   
+}
+
+sing();
